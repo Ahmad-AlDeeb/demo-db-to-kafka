a16762e2b6154dc0e40deaa6bf7ae4970652f5f9
Ahmad-AlDeeb <ahmeddeeb22222@gmail.com>
2024-09-10 14:01:21 +0300

    Configure Checkstyle

--- src/main/java/com/ittovative/demodbtokafka/DemoDbToKafkaApplication.java
+++ src/main/java/com/ittovative/demodbtokafka/DemoDbToKafkaApplication.java
@@ -3,9 +3,18 @@ package com.ittovative.demodbtokafka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The type Demo db to kafka application.
+ */
+@SuppressWarnings("checkstyle:HideUtilityClassConstructor")
 @SpringBootApplication
 public class DemoDbToKafkaApplication {
 
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
         SpringApplication.run(DemoDbToKafkaApplication.class, args);
     }

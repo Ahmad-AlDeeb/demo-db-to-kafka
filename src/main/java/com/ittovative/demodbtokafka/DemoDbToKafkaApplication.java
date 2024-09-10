af204d63d91f7ce04a4415064960e3306bbce951
Ahmad-AlDeeb <ahmeddeeb22222@gmail.com>
2024-09-10 13:40:32 +0300

    Initial commit

---
+++ src/main/java/com/ittovative/demodbtokafka/DemoDbToKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.ittovative.demodbtokafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDbToKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoDbToKafkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ittovative/demodbtokafka/DemoDbToKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ittovative.demodbtokafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDbToKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

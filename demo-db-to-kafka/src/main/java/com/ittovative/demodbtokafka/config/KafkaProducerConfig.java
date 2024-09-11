71bd2435e6a56b1a5f9f16d665ab6ac2f50f2fd7
Ahmad-AlDeeb <ahmeddeeb22222@gmail.com>
2024-09-11 07:16:31 +0300

    Add and configure Kafka

---
+++ demo-db-to-kafka/src/main/java/com/ittovative/demodbtokafka/config/KafkaProducerConfig.java
@@ -0,0 +1,58 @@
+package com.ittovative.demodbtokafka.config;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.ittovative.demodbtokafka.constant.Producer.ACKS;
+import static com.ittovative.demodbtokafka.constant.Producer.BATCH_SIZE;
+import static com.ittovative.demodbtokafka.constant.Producer.BOOTSTRAP_SERVERS;
+import static com.ittovative.demodbtokafka.constant.Producer.BUFFER_MEMORY;
+import static com.ittovative.demodbtokafka.constant.Producer.KEY_SERIALIZER;
+import static com.ittovative.demodbtokafka.constant.Producer.LINGER_MS;
+import static com.ittovative.demodbtokafka.constant.Producer.RETRIES;
+import static com.ittovative.demodbtokafka.constant.Producer.VALUE_SERIALIZER;
+
+
+/**
+ * Configuration for the Kafka producer.
+ */
+@Configuration
+public class KafkaProducerConfig {
+    /**
+     * KafkaTemplate for student.
+     *
+     * @return the kafka template
+     */
+    @Bean
+    public KafkaTemplate<String, String> studentKafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+    /**
+     * Create and configure a Producer.
+     *
+     * @return the producer factory
+     */
+    @Bean
+    public ProducerFactory<String, String> producerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, KEY_SERIALIZER);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, VALUE_SERIALIZER);
+        configProps.put(ProducerConfig.ACKS_CONFIG, ACKS);
+        configProps.put(ProducerConfig.RETRIES_CONFIG, RETRIES);
+        configProps.put(ProducerConfig.BATCH_SIZE_CONFIG, BATCH_SIZE);
+        configProps.put(ProducerConfig.LINGER_MS_CONFIG, LINGER_MS);
+        configProps.put(ProducerConfig.BUFFER_MEMORY_CONFIG, BUFFER_MEMORY);
+
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+}
---
+++ demo-db-to-kafka/src/main/java/com/ittovative/demodbtokafka/constant/Producer.java
@@ -0,0 +1,15 @@
+package com.ittovative.demodbtokafka.constant;
+
+public final class Producer {
+    public static final String BOOTSTRAP_SERVERS = "localhost:19092";
+    public static final String KEY_SERIALIZER = "org.apache.kafka.common.serialization.StringSerializer";
+    public static final String VALUE_SERIALIZER = "org.apache.kafka.common.serialization.StringSerializer";
+    public static final String ACKS = "all";
+    public static final Integer RETRIES = 0;
+    public static final Integer BATCH_SIZE = 16384;
+    public static final Integer LINGER_MS = 1;
+    public static final Integer BUFFER_MEMORY = 33554432;
+
+    private Producer() {
+    }
+}
